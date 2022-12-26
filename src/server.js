7b07ee6e1fdc89c9a98c486ea67cad663847f152
seedjin298 <78636647+seedjin298@users.noreply.github.com>
2022-12-26 14:19:20 +0000

    #3.5 Middlewares part One

--- src/server.js
+++ src/server.js
@@ -4,15 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("I still love you.")
-}
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
 }
 
-app.get("/", handleHome);
-app.get("login", handleLogin);
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+}
+
+app.get("/", gossipMiddleware, handleHome)
  
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
