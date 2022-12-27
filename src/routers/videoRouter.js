3f6a628fd13b0fc84f7aedc18e227ed0af1eb506
seedjin298 <78636647+seedjin298@users.noreply.github.com>
2022-12-27 14:22:23 +0000

    #6.5 More Practice part One making upload

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { watch, upload, deleteVideo, getEdit, postEdit } from "../controllers/videoController";
+import { watch, getUpload, postUpload, getEdit, postEdit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
@@ -7,5 +7,6 @@ videoRouter.get("/:id(\\d+)", watch);
 // videoRouter.get("/:id(\\d+)/edit", getEdit);
 // videoRouter.post("/:id(\\d+)/edit", postEdit);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
