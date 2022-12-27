385a0c8040c828b2716eb419b1a274e13e7d50a5
seedjin298 <78636647+seedjin298@users.noreply.github.com>
2022-12-27 06:24:53 +0000

    #5.8 Iteration each a in b else

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,17 @@
-const fakeUser = {
-  username: "Nicolas",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Hello",
+    },
+    {
+      title: "Video #2",
+    },
+    {
+      title: "Whatsup",
+    },
+  ];
+  return res.render("home", {pagetitle: "Home", videos})
 };
-
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch")
 export const edit = (req, res) => res.render("edit")
 export const search = (req, res) => res.send("search");
