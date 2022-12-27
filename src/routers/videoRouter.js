fa5aaa069dd5c34c5aade47a0a892e0a0f0d32ee
seedjin298 <78636647+seedjin298@users.noreply.github.com>
2022-12-27 05:22:38 +0000

    #4.3 Exports made controllers export vs export default

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
+import { watch, edit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/video", handleWatchVideo);
+videoRouter.get("/watch", watch);
+videoRouter.get("/edit", edit)
 
 export default videoRouter;
