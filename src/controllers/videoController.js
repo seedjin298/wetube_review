2ae48bd880e8b2a26056d2e3936b850322d629f3
seedjin298 <78636647+seedjin298@users.noreply.github.com>
2022-12-27 13:58:53 +0000

    #6.1 Array Database part Two absolute url, relative url

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
     rating: 5,
     comments: 2,
     createdAt: "2 minutes ago",
-    views: 59,
+    views: 1,
     id: 1,
   },
   {
@@ -26,12 +26,12 @@ let videos = [
 ];
 
 export const trending = (req, res) => {
-  return res.render("home", { pagetitle: "Home", videos })
+  return res.render("home", { pageTitle: "Home", videos })
 };
-export const see = (req, res) => {
+export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 export const edit = (req, res) => res.render("edit")
 export const search = (req, res) => res.send("search");
