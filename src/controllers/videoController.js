a459c9bdb57209940d5ef66c56503e5a75cbfd7f
seedjin298 <78636647+seedjin298@users.noreply.github.com>
2022-12-27 05:42:03 +0000

    #4.8 URL Parameters part One req.params req.params.id

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,10 @@
 export const trending = (req, res) => res.send("home page videos");
-export const see = (req, res) => res.send("watch");
-export const edit = (req, res) => res.send("edit");
+export const see = (req, res) => {
+  return res.send(`Watch Video #${req.params.id}`)
+};
+export const edit = (req, res) => {
+  return res.send("edit")
+};
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => res.send("delete video");
