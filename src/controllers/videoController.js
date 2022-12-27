74d74a45e51812d77b7c7f4ed0f9b8c25c6ae0e1
seedjin298 <78636647+seedjin298@users.noreply.github.com>
2022-12-27 05:31:26 +0000

    #4.6 Planning Routes added more routes

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,6 @@
 export const trending = (req, res) => res.send("home page videos");
-export const watch = (req, res) => res.send("watch");
+export const see = (req, res) => res.send("watch");
 export const edit = (req, res) => res.send("edit");
+export const search = (req, res) => res.send("search");
+export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => res.send("delete video");
